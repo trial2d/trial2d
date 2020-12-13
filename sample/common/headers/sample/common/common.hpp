5587eaf2c49497c0aed4fc6dd84a9674dca60714
BlackMATov <blackmat@mail.ru>
2020-12-13 07:29:05 +0700

    separate sample to common, editor and launch parts

---
+++ sample/common/headers/sample/common/common.hpp
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * This file is part of the "https://github.com/trial2d/trial2d.hpp"
+ * For conditions of distribution and use, see copyright notice in LICENSE.md
+ * Copyright (C) 2020, by Matvey Cherevko (blackmatov@gmail.com)
+ ******************************************************************************/
+
+#pragma once
+
+#include <trial2d/trial2d.hpp>
+
+#include <trial2d/inspector/inspector.hpp>
+#include <trial2d/particles/particles.hpp>
+#include <trial2d/physics/physics.hpp>
+#include <trial2d/widgets/widgets.hpp>
+
+namespace sample::common
+{
+}
