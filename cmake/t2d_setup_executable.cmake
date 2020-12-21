eeb7f43a2deaff960694a87dffc4af7cef999a1b
BlackMATov <blackmat@mail.ru>
2020-12-22 05:30:01 +0700

    T2D_SETUP_EXECUTABLE cmake script

---
+++ cmake/t2d_setup_executable.cmake
@@ -0,0 +1,40 @@
+function(T2D_SETUP_EXECUTABLE TARGET)
+    cmake_parse_arguments(
+        PARSE_ARGV
+        1
+        ARG
+        ""
+        "TEMPLATES_DIR;BUNDLE_IDENTIFIER"
+        "")
+
+    if(NOT ARG_TEMPLATES_DIR)
+        message(FATAL_ERROR "Missing argument: TEMPLATES_DIR, TARGET: ${TARGET}")
+    endif()
+
+    if(NOT ARG_BUNDLE_IDENTIFIER)
+        message(FATAL_ERROR "Missing argument: BUNDLE_IDENTIFIER, TARGET: ${TARGET}")
+    endif()
+
+    if(APPLE)
+        if(IOS)
+            set_target_properties(${TARGET} PROPERTIES
+                MACOSX_BUNDLE ON
+                MACOSX_BUNDLE_GUI_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER}
+                MACOSX_BUNDLE_INFO_PLIST "${ARG_TEMPLATES_DIR}/ios.plist"
+                XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER})
+        else()
+            set_target_properties(${TARGET} PROPERTIES
+                MACOSX_BUNDLE ON
+                MACOSX_BUNDLE_GUI_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER}
+                MACOSX_BUNDLE_INFO_PLIST "${ARG_TEMPLATES_DIR}/cocoa.plist"
+                XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER})
+        endif()
+    elseif(EMSCRIPTEN)
+        set_target_properties(${TARGET} PROPERTIES
+            SUFFIX ".html")
+        target_link_options(${TARGET}
+            PRIVATE -sFILESYSTEM=0 -sMALLOC=emmalloc
+            PRIVATE --shell-file "${ARG_TEMPLATES_DIR}/wasm.html"
+            PRIVATE $<$<NOT:$<CONFIG:Debug>>:-sASSERTIONS=0 --closure=1>)
+    endif()
+endfunction()
