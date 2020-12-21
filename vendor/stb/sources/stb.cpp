73f085b1fb995504f70c2acfea79bf94ce9f40d1
BlackMATov <blackmat@mail.ru>
2020-12-21 10:32:42 +0700

    add imgui vendor library, remove some git submodules (sokol, stb)

--- vendor/stb/sources/stb.cpp
+++ vendor/stb/sources/stb.cpp
@@ -4,20 +4,14 @@
  * Copyright (C) 2020, by Matvey Cherevko (blackmatov@gmail.com)
  ******************************************************************************/
 
-//
-// stb_image
-//
-
-#define STBI_ONLY_PNG
-#define STBI_ONLY_TGA
-#define STBI_ONLY_JPEG
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 
-//
-// stb_image_write
-//
+#define STB_RECT_PACK_IMPLEMENTATION
+#include <stb/stb_rect_pack.h>
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb/stb_image_write.h>
+#define STB_TRUETYPE_IMPLEMENTATION
+#include <stb/stb_truetype.h>
