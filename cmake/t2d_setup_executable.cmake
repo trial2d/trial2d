44bd53af082619e05670ec90cd15748c9cefdf7b
BlackMATov <blackmat@mail.ru>
2020-12-24 06:39:59 +0700

    add -pic for ios and mac

--- cmake/t2d_setup_executable.cmake
+++ cmake/t2d_setup_executable.cmake
@@ -15,27 +15,33 @@ function(T2D_SETUP_EXECUTABLE TARGET)
         message(FATAL_ERROR "Missing argument: BUNDLE_IDENTIFIER, TARGET: ${TARGET}")
     endif()
 
-    if(WIN32)
+    if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
         set_target_properties(${TARGET} PROPERTIES
-            WIN32_EXECUTABLE ON)
-    elseif(APPLE)
-        if(IOS)
-            set_target_properties(${TARGET} PROPERTIES
-                MACOSX_BUNDLE ON
-                MACOSX_BUNDLE_GUI_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER}
-                MACOSX_BUNDLE_INFO_PLIST "${ARG_TEMPLATES_DIR}/ios.plist"
-                XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER})
-        else()
-            set_target_properties(${TARGET} PROPERTIES
-                MACOSX_BUNDLE ON
-                MACOSX_BUNDLE_GUI_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER}
-                MACOSX_BUNDLE_INFO_PLIST "${ARG_TEMPLATES_DIR}/cocoa.plist"
-                XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER})
-        endif()
-    elseif(EMSCRIPTEN)
+            MACOSX_BUNDLE ON
+            POSITION_INDEPENDENT_CODE ON
+            MACOSX_BUNDLE_GUI_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER}
+            MACOSX_BUNDLE_INFO_PLIST ${ARG_TEMPLATES_DIR}/cocoa.plist
+            XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER})
+    endif()
+
+    if(CMAKE_SYSTEM_NAME MATCHES "iOS")
+        set_target_properties(${TARGET} PROPERTIES
+            MACOSX_BUNDLE ON
+            POSITION_INDEPENDENT_CODE ON
+            MACOSX_BUNDLE_GUI_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER}
+            MACOSX_BUNDLE_INFO_PLIST ${ARG_TEMPLATES_DIR}/ios.plist
+            XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${ARG_BUNDLE_IDENTIFIER})
+    endif()
+
+    if(CMAKE_SYSTEM_NAME MATCHES "Emscripten")
         set_target_properties(${TARGET} PROPERTIES
             SUFFIX ".html")
         target_link_options(${TARGET}
-            PRIVATE --shell-file "${ARG_TEMPLATES_DIR}/wasm.html")
+            PRIVATE --shell-file ${ARG_TEMPLATES_DIR}/wasm.html)
+    endif()
+
+    if(CMAKE_SYSTEM_NAME MATCHES "Windows")
+        set_target_properties(${TARGET} PROPERTIES
+            WIN32_EXECUTABLE ON)
     endif()
 endfunction()
