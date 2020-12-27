312231174f1fa22a7b6b137cef6f5b401845a616
BlackMATov <blackmat@mail.ru>
2020-12-27 13:30:56 +0700

    prepare_android script

--- .ci/build_debug_android.sh
+++ .ci/build_debug_android.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
-BUILD_DIR=`dirname "$BASH_SOURCE"`/../build
+DIR=`dirname "${BASH_SOURCE}"`
+
+BUILD_DIR="${DIR}/../build"
+ANDROID_NDK_DIR="${DIR}/android_sdk/ndk/21.1.6352462"
+
+if [ ! -d "${ANDROID_NDK_DIR}" ]; then
+    "${DIR}/prepare_android.sh"
+fi
+
 mkdir -p $BUILD_DIR/debug_android
 pushd $BUILD_DIR/debug_android
 cmake ../.. -DCMAKE_BUILD_TYPE=Debug\
-    -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake\
-    -DANDROID_PLATFORM=android-19\
-    -DANDROID_ABI=armeabi-v7a
+    -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_DIR}/build/cmake/android.toolchain.cmake"\
+    -DANDROID_ABI=armeabi-v7a\
+    -DANDROID_PLATFORM=android-19
 cmake --build .
 popd
---
+++ .ci/prepare_android.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -e
+
+CMDLINE_TOOLS_ID=6858069
+BUILD_TOOLS_VERSION=30.0.3
+ANDROID_PLATFORM_VERSION=30
+NDK_VERSION=21.1.6352462
+
+#
+#
+#
+
+DIR=`dirname "${BASH_SOURCE}"`
+
+DOWNLOADS_DIR="${DIR}/downloads"
+ANDROID_SDK_DIR="${DIR}/android_sdk"
+
+mkdir -p "${DOWNLOADS_DIR}"
+mkdir -p "${ANDROID_SDK_DIR}"
+
+#
+#
+#
+
+SYSTEM_NAME=`uname | tr '[:upper:]' '[:lower:]'`
+case "${SYSTEM_NAME}" in
+    "darwin") CMDLINE_TOOLS_NAME="commandlinetools-mac-${CMDLINE_TOOLS_ID}_latest";;
+    "linux") CMDLINE_TOOLS_NAME="commandlinetools-linux-${CMDLINE_TOOLS_ID}_latest";;
+    "windows") CMDLINE_TOOLS_NAME="commandlinetools-win-${CMDLINE_TOOLS_ID}_lates"t;;
+esac
+
+CMDLINE_TOOLS_URL="https://dl.google.com/android/repository/${CMDLINE_TOOLS_NAME}.zip"
+CMDLINE_TOOLS_URL_FILENAME="${DOWNLOADS_DIR}/cmdline-tools-${CMDLINE_TOOLS_ID}.zip"
+
+#
+#
+#
+
+if [ ! -e "${CMDLINE_TOOLS_URL_FILENAME}" ]; then
+    echo "Downloading cmdline-tools: ${CMDLINE_TOOLS_URL_FILENAME}"
+    wget -q "${CMDLINE_TOOLS_URL}" -O "${CMDLINE_TOOLS_URL_FILENAME}"
+else
+    echo "Found downloaded package:" ${CMDLINE_TOOLS_URL_FILENAME}
+fi
+
+CMDLINE_TOOLS_DIR="${ANDROID_SDK_DIR}/cmdline-tools"
+
+if [ ! -d "${CMDLINE_TOOLS_DIR}" ]; then
+    echo "Unpacking cmdline-tools: ${CMDLINE_TOOLS_DIR}"
+    unzip -q "${CMDLINE_TOOLS_URL_FILENAME}" -d "${ANDROID_SDK_DIR}"
+else
+    echo "Found unpacked cmdline-tools: ${CMDLINE_TOOLS_DIR}"
+fi
+
+#
+#
+#
+
+echo "Installing android sdk packages: ${ANDROID_SDK_DIR}"
+
+(echo y | "${CMDLINE_TOOLS_DIR}/bin/sdkmanager" --sdk_root="${ANDROID_SDK_DIR}" "extras;android;m2repository")
+(echo y | "${CMDLINE_TOOLS_DIR}/bin/sdkmanager" --sdk_root="${ANDROID_SDK_DIR}" "build-tools;${BUILD_TOOLS_VERSION}")
+(echo y | "${CMDLINE_TOOLS_DIR}/bin/sdkmanager" --sdk_root="${ANDROID_SDK_DIR}" "platforms;android-${ANDROID_PLATFORM_VERSION}")
+(echo y | "${CMDLINE_TOOLS_DIR}/bin/sdkmanager" --sdk_root="${ANDROID_SDK_DIR}" "ndk;${NDK_VERSION}")
+
+rm -rf "${ANDROID_SDK_DIR}/emulator"
