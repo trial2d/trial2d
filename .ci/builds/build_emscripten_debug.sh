50f482a2cc71e2498d6cf4ea12f39a48d3c9755b
BlackMATov <blackmat@mail.ru>
2020-12-31 08:58:23 +0700

    little style fixes

--- .ci/builds/build_emscripten_debug.sh
+++ .ci/builds/build_emscripten_debug.sh
@@ -4,8 +4,8 @@ DIR=`dirname "${BASH_SOURCE}"`
 
 PWD_DIR=`pwd`
 ROOT_DIR="${DIR}/../.."
-BUILD_DIR="${ROOT_DIR}/build"
 PLATFORM_DIR="${DIR}/../platform"
+BUILD_DIR="${ROOT_DIR}/build/emscripten_debug"
 EMSDK_DIR="${PLATFORM_DIR}/emscripten_sdk/emsdk-2.0.11"
 
 if [ ! -d "${EMSDK_DIR}" ]; then
@@ -14,8 +14,7 @@ fi
 
 source "${EMSDK_DIR}/emsdk_env.sh"
 
-mkdir -p "${BUILD_DIR}/emscripten_debug"
-pushd "${BUILD_DIR}/emscripten_debug"
+mkdir -p "${BUILD_DIR}" && pushd "${BUILD_DIR}"
 emcmake cmake "${PWD_DIR}/${ROOT_DIR}" -DCMAKE_BUILD_TYPE=Debug
 cmake --build .
 popd
--- .ci/builds/build_unix_release.sh
+++ .ci/builds/build_unix_release.sh
@@ -4,10 +4,9 @@ DIR=`dirname "${BASH_SOURCE}"`
 
 PWD_DIR=`pwd`
 ROOT_DIR="${DIR}/../.."
-BUILD_DIR="${ROOT_DIR}/build"
+BUILD_DIR="${ROOT_DIR}/build/unix_release"
 
-mkdir -p "$BUILD_DIR/unix_release"
-pushd "$BUILD_DIR/unix_release"
+mkdir -p "$BUILD_DIR" && pushd "$BUILD_DIR"
 cmake "${PWD_DIR}/${ROOT_DIR}" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 ctest --verbose
