b0301fe4f14a8614b9ed378ca09dee67c011a0d4
BlackMATov <blackmat@mail.ru>
2020-12-31 07:38:27 +0700

    new emscripten build scripts

---
+++ .ci/builds/build_emscripten_debug.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+DIR=`dirname "${BASH_SOURCE}"`
+
+PWD_DIR=`pwd`
+ROOT_DIR="${DIR}/../.."
+BUILD_DIR="${ROOT_DIR}/build"
+PLATFORM_DIR="${DIR}/../platform"
+EMSDK_DIR="${PLATFORM_DIR}/emscripten_sdk/emsdk-2.0.11"
+
+if [ ! -d "${EMSDK_DIR}" ]; then
+    "${PLATFORM_DIR}/prepare_emscripten.sh"
+fi
+
+source "${EMSDK_DIR}/emsdk_env.sh"
+
+mkdir -p "${BUILD_DIR}/emscripten_debug"
+pushd "${BUILD_DIR}/emscripten_debug"
+emcmake cmake "${PWD_DIR}/${ROOT_DIR}" -DCMAKE_BUILD_TYPE=Debug
+cmake --build .
+popd
