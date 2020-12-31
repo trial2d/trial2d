bbba6bb57e222129f6c8ec00fbc365e88498b995
BlackMATov <blackmat@mail.ru>
2020-12-31 07:46:02 +0700

    new unix build scripts

--- .ci/build_release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-BUILD_DIR=`dirname "$BASH_SOURCE"`/../build
-mkdir -p $BUILD_DIR/release
-pushd $BUILD_DIR/release
-cmake -DCMAKE_BUILD_TYPE=Release ../..
-cmake --build .
-ctest --verbose
-popd
---
+++ .ci/builds/build_unix_debug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+DIR=`dirname "${BASH_SOURCE}"`
+
+PWD_DIR=`pwd`
+ROOT_DIR="${DIR}/../.."
+BUILD_DIR="${ROOT_DIR}/build"
+
+mkdir -p "$BUILD_DIR/unix_debug"
+pushd "$BUILD_DIR/unix_debug"
+cmake "${PWD_DIR}/${ROOT_DIR}" -DCMAKE_BUILD_TYPE=Debug
+cmake --build .
+ctest --verbose
+popd
