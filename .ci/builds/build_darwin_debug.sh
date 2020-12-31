f5d008e65fc058b8a16b30d6284269b567a07fa6
BlackMATov <blackmat@mail.ru>
2020-12-31 10:32:46 +0700

    build darwin projects without current directory changes

--- .ci/builds/build_darwin_debug.sh
+++ .ci/builds/build_darwin_debug.sh
@@ -2,12 +2,10 @@
 set -e
 DIR=`dirname "${BASH_SOURCE}"`
 
-PWD_DIR=`pwd`
 ROOT_DIR="${DIR}/../.."
 BUILD_DIR="${ROOT_DIR}/build/darwin_debug"
 
-mkdir -p "$BUILD_DIR" && pushd "$BUILD_DIR"
-cmake "${PWD_DIR}/${ROOT_DIR}" -DCMAKE_BUILD_TYPE=Debug
-cmake --build .
-ctest --verbose
-popd
+mkdir -p "$BUILD_DIR"
+cmake "${ROOT_DIR}" -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Debug
+cmake --build "$BUILD_DIR"
+(cd "$BUILD_DIR" && ctest --verbose)
