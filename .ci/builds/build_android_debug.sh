e2794b78459ebcd46cbc8af02103792ccb92aa06
BlackMATov <blackmat@mail.ru>
2020-12-31 07:26:04 +0700

    new android build scripts

---
+++ .ci/builds/build_android_debug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+DIR=`dirname "${BASH_SOURCE}"`
+
+SAMPLE_DIR="${DIR}/../../sample"
+PLATFORM_DIR="${DIR}/../platform"
+ANDROID_NDK_DIR="${PLATFORM_DIR}/android_sdk/ndk/21.1.6352462"
+
+if [ ! -d "${ANDROID_NDK_DIR}" ]; then
+    "${PLATFORM_DIR}/prepare_android.sh"
+fi
+
+pushd "${SAMPLE_DIR}/gradle"
+gradle assembleDebug
+popd
