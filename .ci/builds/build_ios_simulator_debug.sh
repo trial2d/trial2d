3b015df044781f595373e0b8a9c8a265e8489c4b
BlackMATov <blackmat@mail.ru>
2021-01-02 10:47:13 +0700

    ios build scripts

---
+++ .ci/builds/build_ios_simulator_debug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+DIR=`dirname "${BASH_SOURCE}"`
+
+PWD_DIR=`pwd`
+ROOT_DIR="${DIR}/../.."
+BUILD_DIR="${ROOT_DIR}/build/ios_simulator_debug"
+
+mkdir -p "${BUILD_DIR}"
+(cd "${BUILD_DIR}" && cmake "${PWD_DIR}/${ROOT_DIR}"\
+    -DCMAKE_BUILD_TYPE=Debug\
+    -DCMAKE_TOOLCHAIN_FILE="${PWD_DIR}/${ROOT_DIR}/cmake/ios.toolchain.cmake"\
+    -DPLATFORM=SIMULATOR64\
+    -DDEPLOYMENT_TARGET=10.0)
+(cd "${BUILD_DIR}" && cmake --build .)
