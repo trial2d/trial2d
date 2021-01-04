016eac6198472d80ac0f89484df3d8dc4b06b325
BlackMATov <blackmat@mail.ru>
2021-01-04 21:54:11 +0700

    move ios-cmake toolchain to vendor

--- .ci/builds/build_ios_device.sh
+++ .ci/builds/build_ios_device.sh
@@ -9,7 +9,7 @@ BUILD_DIR="${ROOT_DIR}/build/ios_device"
 mkdir -p "${BUILD_DIR}"
 (cd "${BUILD_DIR}" && cmake "${PWD_DIR}/${ROOT_DIR}"\
     -DCMAKE_BUILD_TYPE=Release\
-    -DCMAKE_TOOLCHAIN_FILE="${PWD_DIR}/${ROOT_DIR}/cmake/ios.toolchain.cmake"\
+    -DCMAKE_TOOLCHAIN_FILE="${PWD_DIR}/${ROOT_DIR}/vendor/ios-cmake/ios.toolchain.cmake"\
     -DPLATFORM=OS\
     -DDEPLOYMENT_TARGET=10.0)
 (cd "${BUILD_DIR}" && cmake --build .)
